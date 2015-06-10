524975209013dd89b9e0a2832d6bc3033bf26122
BourbonZ <zhwezhwe@163.com>
2015-06-10 22:30:02 +0800

    绘制WWDC2015icon
    绘制WWDC2015icon

---
+++ Human/Circle.h
@@ -0,0 +1,13 @@
+//
+//  Circle.h
+//  Human
+//
+//  Created by BourbonZ on 15/6/10.
+//  Copyright (c) 2015年 Bourbon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Circle : UIView
+
+@end
--- Human/HumanView.m
+++ Human/HumanView.m
@@ -9,6 +9,8 @@
 #import "HumanView.h"
 #define r 100
 #import <CoreGraphics/CoreGraphics.h>
+#import "Circle.h"
+#define Color(r,g,b,a) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:a]
 @implementation HumanView
 
 
@@ -19,12 +21,56 @@
     
     ///获取当前图形上下文
     CGContextRef context = UIGraphicsGetCurrentContext();
+//
+//    
+//    CGContextTranslateCTM(context,self.frame.size.width/2, self.frame.size.height/2);
+//
+//
+//    NSMutableArray *array = [NSMutableArray array];
+//    UIColor *color1 = Color(16,63,236,0.75);
+//    UIColor *color2 = Color(37,172,162,0.75);
+//    UIColor *color3 = Color(233,124,32,0.75);
+//    UIColor *color4 = Color(235,67,35,0.75);
+//    UIColor *color5 = Color(190,28,65,0.75);
+//    UIColor *color6 = Color(208,57,159,0.75);
+//    UIColor *color7 = Color(150,32,198,0.75);
+//    UIColor *color8 = Color(95,33,203,0.75);
+//    
+//    [array addObject:color1];
+//    [array addObject:color2];
+//    [array addObject:color3];
+//    [array addObject:color4];
+//    [array addObject:color5];
+//    [array addObject:color6];
+//    [array addObject:color7];
+//    [array addObject:color8];
+//    
+//    for (int i = 0; i < 8; i ++)
+//    {
+//        UIColor *tmp = [array objectAtIndex:i];
+//        CGContextAddEllipseInRect(context, CGRectMake(-80, 0, 180, 180));
+//        CGContextSetFillColorWithColor(context, tmp.CGColor);
+//        CGContextRotateCTM(context,  M_PI_4);
+//        
+//        
+//        CGContextFillPath(context);
+
+//        CGContextRotateCTM(context, M_PI * 90 / 360);
+//        
+//        CGContextAddEllipseInRect(context, rect);
+//        //    CGContextSetFillColorWithColor(context, color.CGColor);
+//        CGContextFillPath(context);
+
+//    }
+    
+    
+    
+    
     
     drawBody(context,rect);
     drawEyy(context,rect);
     drawMouth(context,rect);
 }
-
 ///画身体
 void drawBody(CGContextRef context,CGRect rect)
 {
@@ -114,4 +160,5 @@ void drawEyy(CGContextRef context,CGRect rect)
     
 
 }
+
 @end
